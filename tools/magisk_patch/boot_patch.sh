b463dd78c62e96ec3761eeb87412358cd1d46a3e
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-09-30 22:54:07 +0800

    Update Magisk 26.3

--- tools/magisk_patch/boot_patch.sh
+++ tools/magisk_patch/boot_patch.sh
@@ -5,18 +5,15 @@
 #
 # Usage: boot_patch.sh <bootimage>
 #
-# The following flags can be set in environment variables:
-# KEEPVERITY, KEEPFORCEENCRYPT, PATCHVBMETAFLAG, RECOVERYMODE, SYSTEM_ROOT
+# The following environment variables can configure the installation:
+# KEEPVERITY, KEEPFORCEENCRYPT, PATCHVBMETAFLAG, RECOVERYMODE, LEGACYSAR
 #
 # This script should be placed in a directory with the following files:
 #
 # File name          Type      Description
 #
-# boot_patch.sh      script    A script to patch boot image for Magisk.
-#                  (this file) The script will use files in its same
-#                              directory to complete the patching process.
+# boot_patch.sh      script    A script to patch boot image for Magisk. The script will use files in its same directory to complete the patching process.
 # magiskinit         binary    The binary to replace /init.
-# magisk32           binary    The magisk binaries.
 # magisk64           binary    The magisk binaries.
 # magiskboot         binary    A tool to manipulate boot images.
 # stub.apk           binary    The stub Magisk app to embed into ramdisk.
@@ -42,6 +39,10 @@ getdir() {
   esac
 }
 
+#################
+# Initialization
+#################
+
 ui_print() {
   echo "$1"
 }
@@ -50,15 +51,6 @@ abort() {
   ui_print "$1"
   exit 1
 }
-
-#################
-# Initialization
-#################
-
-if [ -z $SOURCEDMODE ]; then
-  # Switch to the location of the script file
-  cd "$(getdir "${BASH_SOURCE:-$0}")"
-fi
 
 BOOTIMAGE="$1"
 [ -e "$BOOTIMAGE" ] || abort "$BOOTIMAGE does not exist!"
@@ -75,7 +67,7 @@ fi
 [ -z $KEEPFORCEENCRYPT ] && KEEPFORCEENCRYPT=false
 [ -z $PATCHVBMETAFLAG ] && PATCHVBMETAFLAG=false
 [ -z $RECOVERYMODE ] && RECOVERYMODE=false
-[ -z $SYSTEM_ROOT ] && SYSTEM_ROOT=false
+[ -z $LEGACYSAR ] && LEGACYSAR=false
 export KEEPVERITY
 export KEEPFORCEENCRYPT
 export PATCHVBMETAFLAG
@@ -86,8 +78,6 @@ chmod -R 755 .
 # Unpack
 #########
 
-CHROMEOS=false
-
 ui_print "- Unpacking boot image"
 ./magiskboot unpack "$BOOTIMAGE"
 
@@ -95,10 +85,6 @@ case $? in
   0 ) ;;
   1 )
     abort "! Unsupported/Unknown image format"
-    ;;
-  2 )
-    ui_print "- ChromeOS boot image detected"
-    CHROMEOS=true
     ;;
   * )
     abort "! Unable to unpack boot image"
@@ -114,9 +100,11 @@ ui_print "- Checking ramdisk status"
 if [ -e ramdisk.cpio ]; then
   ./magiskboot cpio ramdisk.cpio test
   STATUS=$?
+  SKIP_BACKUP=""
 else
   # Stock A only legacy SAR, or some Android 13 GKIs
   STATUS=0
+  SKIP_BACKUP="#"
 fi
 case $((STATUS & 3)) in
   0 )  # Stock boot
@@ -127,9 +115,9 @@ case $((STATUS & 3)) in
     ;;
   1 )  # Magisk patched
     ui_print "- Magisk patched boot image detected"
-    # Find SHA1 of stock boot image
-    [ -z $SHA1 ] && SHA1=$(./magiskboot cpio ramdisk.cpio sha1 2>/dev/null)
-    ./magiskboot cpio ramdisk.cpio restore
+    ./magiskboot cpio ramdisk.cpio \
+    "extract .backup/.magisk config.orig" \
+    "restore"
     cp -af ramdisk.cpio ramdisk.cpio.orig
     rm -f stock_boot.img
     ;;
@@ -152,41 +140,31 @@ fi
 ui_print "- Patching ramdisk"
 
 # Compress to save precious ramdisk space
-SKIP32="#"
 SKIP64="#"
 if [ -f magisk64 ]; then
   PREINITDEVICE=metadata
   ./magiskboot compress=xz magisk64 magisk64.xz
   unset SKIP64
 fi
-if [ -f magisk32 ]; then
-  PREINITDEVICE=metadata
-  ./magiskboot compress=xz magisk32 magisk32.xz
-  unset SKIP32
-fi
 ./magiskboot compress=xz stub.apk stub.xz
 
 echo "KEEPVERITY=$KEEPVERITY" > config
 echo "KEEPFORCEENCRYPT=$KEEPFORCEENCRYPT" >> config
-echo "PATCHVBMETAFLAG=$PATCHVBMETAFLAG" >> config
 echo "RECOVERYMODE=$RECOVERYMODE" >> config
 if [ -n "$PREINITDEVICE" ]; then
-  ui_print "- Pre-init storage partition device ID: $PREINITDEVICE"
+  ui_print "- Pre-init storage partition: $PREINITDEVICE"
   echo "PREINITDEVICE=$PREINITDEVICE" >> config
 fi
 [ -n "$SHA1" ] && echo "SHA1=$SHA1" >> config
-RANDOMSEED=$(tr -dc 'a-f0-9' < /dev/urandom | head -c 16)
-echo "RANDOMSEED=0x$RANDOMSEED" >> config
 
 ./magiskboot cpio ramdisk.cpio \
 "add 0750 $INIT magiskinit" \
 "mkdir 0750 overlay.d" \
 "mkdir 0750 overlay.d/sbin" \
-"$SKIP32 add 0644 overlay.d/sbin/magisk32.xz magisk32.xz" \
 "$SKIP64 add 0644 overlay.d/sbin/magisk64.xz magisk64.xz" \
 "add 0644 overlay.d/sbin/stub.xz stub.xz" \
 "patch" \
-"backup ramdisk.cpio.orig" \
+"$SKIP_BACKUP backup ramdisk.cpio.orig" \
 "mkdir 000 .backup" \
 "add 000 .backup/.magisk config" \
 || abort "! Unable to patch ramdisk"
@@ -224,7 +202,7 @@ if [ -f kernel ]; then
 
   # Force kernel to load rootfs for legacy SAR devices
   # skip_initramfs -> want_initramfs
-  $SYSTEM_ROOT && ./magiskboot hexpatch kernel \
+  $LEGACYSAR && ./magiskboot hexpatch kernel \
   736B69705F696E697472616D667300 \
   77616E745F696E697472616D667300 \
   && PATCHEDKERNEL=true
@@ -240,9 +218,6 @@ fi
 
 ui_print "- Repacking boot image"
 ./magiskboot repack "$BOOTIMAGE" || abort "! Unable to repack boot image"
-
-# Sign chromeos boot
-$CHROMEOS && sign_chromeos
 
 # Restore the original boot partition path
 [ -e "$BOOTNAND" ] && BOOTIMAGE="$BOOTNAND"
